144779198f49075ea71825ff98debc4fc881c352
programmer <93894129+qwerty83927@users.noreply.github.com>
2021-11-07 18:51:27 -0800

    Update text_functions.c

--- text_functions.c
+++ text_functions.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+// for printing vars
 #define print(x) _Generic(x, int: printf("%i",x), double: printf("%d",x), char*: printf("%s",x), char: printf("%c",x), default: printf("unknown data"));
 
+// for string binding
 char* bind(const char *s1, const char *s2) {
 char *result = malloc(strlen(s1) + strlen(s2) + 1);
 strcpy(result, s1);
